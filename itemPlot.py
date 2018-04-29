05843a1164e8ab81be11887c2a724debb954c532
BDubon <33406715+BDubon@users.noreply.github.com>
2018-04-28 21:43:46 -0400

    Deleted old code

--- itemPlot.py
+++ itemPlot.py
@@ -4,21 +4,6 @@ import matplotlib.pyplot as plt
 prices = []
 
 # gets average of ONLY price column
-'''with open("B007TY6MK2.csv") as file:
-    for line in file:
-        prices.append(line[21:26])
-
-prices.pop(0)
-prices = list(map(float, prices))
-# print(prices)
-
-# max, min, and avg
-max_value = max(prices)
-min_value = min(prices)
-avg_value = sum(prices)/len(prices)
-print(max_value)
-print(min_value)
-print(avg_value)'''
 # read file
 df = pd.read_csv("B007TY6MK2.csv")
 avgPrice = round(df['Price'].mean(), 2)
@@ -29,9 +14,7 @@ print(avgPrice)
 print(hPrice)
 print(lPrice)
 
-
 # ------------- plot section ---------------
-
 
 # set axis
 x = df["Date"]
