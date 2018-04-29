3df6ad4f290e4fc6ef471b50b83026c5d0a67418
BDubon <33406715+BDubon@users.noreply.github.com>
2018-04-29 02:38:26 -0400

    fontsize changes

--- itemPlot.py
+++ itemPlot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # gets average of ONLY price column
 # read file
-df = pd.read_csv("CSVs/B0769MFR7L.csv")
+df = pd.read_csv("CSVs/B010E6VPMG.csv")
 avgPrice = round(df['Price'].mean(), 2)
 hPrice = round(df['Price'].max(), 2)
 lPrice = round(df['Price'].min(), 2)
@@ -19,10 +19,10 @@ x = df["Date"]
 y = df["Price"]
 
 # displays graph
-plt.title('Item Price Over Time')
-plt.xlabel('Date')
+plt.title('Item Price Over Time', fontsize=18)
+plt.xlabel('Date', fontsize=14)
 plt.xticks(rotation=90, fontsize=8)
-plt.ylabel('Price')
+plt.ylabel('Price ($)', fontsize=14)
 plt.plot(x, y)
 plt.show()
 # ------------- plot section ---------------
