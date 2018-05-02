1d49a8328c531ea3b9e2784fdb433ee34148764c
nfuster2017 <33457770+nfuster2017@users.noreply.github.com>
2018-05-02 13:31:34 -0400

    Update MAIN.py
    first attempts at this gueeey

--- MAIN.py
+++ MAIN.py
@@ -61,5 +61,35 @@ with open(asinN + '.csv', 'w') as newWrite:
     newWrite.write(headerRow)
     row = date+ ',' + asinN + ',' + str(price) + ',' + name
     newWrite.write(row)
+    
+    
+# GUI- by Fuster
+from tkinter import *
+from tkinter import ttk
 
+
+
+top = Tk()
+E1 = Entry(top, bd =5)
+def userSelection():
+    userChoice = searchNum.get()
+    if userChoice == 1:
+        funcChoice = submitBtn.bind('<Button-1>', electionDates)
+    if userChoice == 2:
+        funcChoice = submitBtn.bind('<Button-1>', candidatesInfo)
+    return funcChoice
+
+
+L1 = Label(top, text="Product")
+enterProduct = ttk.Label(top, text='Enter Product: ')
+enterProduct.grid(row=0, column=0, sticky=E)
+enterProductbutton = ttk.Entry(top)
+enterProductbutton.grid(row=0, column=1, columnspan=1, sticky=W)
+enterProductbutton.delete(0, END)
+enterProductbutton.insert(0, '')
+submitBtn = ttk.Button(top, text='Submit')
+submitBtn.bind('<Button-1>', userSelection)
+submitBtn.grid(row=3, column=0, columnspan=5, sticky=NSEW)
+
+mainloop()
 
