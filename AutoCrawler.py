e9c6723fd7ed8947e06ae09bc09f1a29b1638013
Raspberry-Jams <archernick2568@gmail.com>
2018-05-12 20:51:19 -0400

    Attempted to automate AutoCrawler.py

--- AutoCrawler.py
+++ AutoCrawler.py
@@ -1,2 +1,25 @@
 from MobileCrawler import asinTracker
+from MobileCrawler import *
 
+new_list = []
+with open('ASINs.txt') as asin:
+    for line in asin:
+        new_list.append(line)
+
+    indiv_asin_set = set(new_list)
+    for asin in indiv_asin_set:
+        soup = pageGet(asin)
+        # Get price
+        try:
+            price = priceGetAll(soup)
+        except:
+            price = priceGetDeal(soup)
+        print('Price:', price)
+
+        # Get product's name
+        try:
+            name = nameGet(soup)
+        except:
+            name = nameGetOther(soup)
+        print('Product Name:', name)
+        csvAppend(asin, price, name)
